7dcdb1c8ac578db972174952ff3aaa40b0cc57c0
tejseth <tejseth@umich.edu>
2021-11-08 07:40:24 -0500

    getting the link

---
+++ mfans-rf-xgboost.R
@@ -0,0 +1,4 @@
+# Tutorial 1: Introduction to nflfastR: https://www.youtube.com/watch?v=uw6PH1YiCtI&t=155s
+# Tutorial 2: Data Viz With nflfastr: https://www.youtube.com/watch?v=tGaXtAAt6-4&t=255s
+# Tutorial 3: Modeling I: https://www.youtube.com/watch?v=J4p8ZfYW5Oo&t=200s
+# Tutorial 4: Modeling II (Today): 
